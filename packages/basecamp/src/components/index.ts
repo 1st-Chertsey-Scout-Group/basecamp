a6adbc1642cbaf1484a7b276477f19e3ed1cd382
Tim Cane <3074314+TimCane@users.noreply.github.com>
2024-12-16 22:00:59 +0000

    initial basecamp library structure

---
+++ packages/basecamp/src/components/index.ts
@@ -0,0 +1,18 @@
+import HeaderNavigation from './header/header-navigation.astro';
+import FooterNavigation from './footer/footer-navigation.astro';
+
+import ColourBlock from './colour-block.astro';
+
+import BlockElement from './block/block-element.astro';
+import BlockHeading from './block/block-heading.astro';
+
+
+import Button from './ui/button.astro';
+import FormSelect from './ui/form-select.astro';
+import FormTextArea from './ui/form-text-area.astro';
+import FormTextBox from './ui/form-text-box.astro';
+
+import Announcement from './announcement.astro';
+import Seperator from './seperator.astro';
+
+export { HeaderNavigation, FooterNavigation, ColourBlock, BlockHeading, BlockElement, Button, FormSelect, FormTextArea, FormTextBox, Announcement, Seperator };
---
+++ packages/basecamp/src/layouts/index.ts
@@ -0,0 +1,4 @@
+import BaseLayout from './base.astro';
+import PageLayout from './page.astro';
+
+export {BaseLayout, PageLayout};
